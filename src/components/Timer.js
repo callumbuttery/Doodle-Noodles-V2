a6d617ac4c3e8fc07896d6413a2dbb7435b3981c
callum buttery <callumbuttery@icloud.com>
2021-10-26 20:34:48 +0100

    Added working timer

---
+++ src/components/Timer.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+class Timer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            day: 0,
+            hour: 0,
+            minute: 0,
+            second: 0
+        };
+
+    }
+    componentDidMount() {
+        setInterval(() => this.calculateTime('November, 21, 2021'), 1000);
+    }
+
+    calculateTime(launchDate) {
+        let day = 0;
+        let hour = 0;
+        let minute = 0;
+        let second = 0;
+        const differenceInDate = Date.parse(launchDate) - Date.parse(new Date());
+
+        if (differenceInDate && differenceInDate > 0) {
+            day = Math.floor(differenceInDate / (1000 * 60 * 60 * 24));
+            hour = Math.floor((differenceInDate / (1000 * 60 * 60)) % 24);
+            minute = Math.floor((differenceInDate / 1000 / 60) % 60);
+            second = Math.floor((differenceInDate / 1000) % 60);
+            this.setState({ day, hour, minute, second });
+        }
+    }
+    render() {
+        return (
+            <div>
+                <div>{this.state.day} : {this.state.hour} : {this.state.minute} : {this.state.second}</div>
+            </div>
+        );
+    }
+}
+export default Timer;
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,13 +1,16 @@
 import logo from '../logo.svg';
 import '../css/welcome.css';
+import Timer from '../components/Timer';
 
 function welcome() {
+
     return (
         <div id='mint'>
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1>Danger Noodles coming soon</h1>
+                    <Timer launchDate='November, 21, 2021'></Timer>
                 </header>
             </div>
         </div>
