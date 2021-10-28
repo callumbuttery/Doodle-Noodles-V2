bdcfc6e9a0ea2f9d1efdc247a34f56bb5df7e9bd
callum buttery <callumbuttery@icloud.com>
2021-10-28 20:59:17 +0100

    what are doodle noodles formatting

--- src/components/About.js
+++ src/components/About.js
@@ -8,14 +8,14 @@ function about() {
                 <img src={WhatAreDoodleNoodles} className='WhatAreDoodleNoodlesLogo'></img>
                 <div className="WhatAreDoodleNoodlesContent">
                     <h1 className='Title'>What are Doodle Noodles?</h1>
-                    <hr style={{ width: '30%', margin: 'auto' }}></hr>
+                    <hr className='hr'></hr>
                     <p className='paragraphFont'>Doodle Noodles are a ~10,000 strong generated NFT project form art
                         made by our resident artist Grin! The images were generated from
                         literally billions of possible combinations
-
-                        Some Noodles are rarer than others and some carry a special Golden
-                        Rune(more information below). Follow our planned Journey by looking
-                        at our roadmap and get on board! We'd love to welcome you int our
+                    </p>
+                    <p className='paragraphFont'>Some Noodles are rarer than others and some carry a special Golden
+                        Rune(more information below).Follow our planned Journey by looking
+                        at our roadmap and get on board!We'd love to welcome you int our
                         new community.
                     </p>
                 </div>
