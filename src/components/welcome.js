a83a8eccdd9fc85ae39c061148e108644f506305
callum buttery <callumbuttery@icloud.com>
2021-10-28 21:27:37 +0100

    update text

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -9,7 +9,7 @@ function welcome() {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h1 style={{ color: '#FF123F' }}>Danger Noodles coming soon</h1>
+                    <h1 style={{ color: '#FF123F' }}>Doodle Noodles coming soon</h1>
                     <Timer launchDate='November, 21, 2021'></Timer>
                 </header>
             </div>
