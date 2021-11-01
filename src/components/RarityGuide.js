0304cb809f84e9075d56c38079463c41de12b883
callum buttery <callumbuttery@icloud.com>
2021-11-01 18:06:38 +0000

    Rarity guide without mobile

--- src/components/RarityGuide.js
+++ src/components/RarityGuide.js
@@ -3,6 +3,24 @@ import '../css/RarityGuide.css'
 function RarityGuide() {
     return (
         <div className="RarityContainer">
+            <div className="RarityTextContainer">
+                <h2 className="RarityGuideTitle">Rarity Guide</h2>
+                <hr className="RarityGuideHR" />
+                <br />
+                <p className="RarityText">Our Rarity system allocates you points by how many
+                    different rarities of Doodle Noodle you hold.
+
+                    <br />
+                    <br />
+                    Each tier assigns you an amount of points, with your
+                    points totals being used in future for giveaways,
+                    bragging rights and competitions in the community.
+
+                    <br />
+                    <br />
+                    The more you collect the more you will be rewarded!
+                </p>
+            </div>
 
         </div>
     )
