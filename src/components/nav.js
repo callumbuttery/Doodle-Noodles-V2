93d0a7642f6959e636b7277699e4070e40109d7d
callum buttery <callumbuttery@icloud.com>
2021-11-03 20:34:07 +0000

    fixed team button

--- src/components/nav.js
+++ src/components/nav.js
@@ -16,7 +16,7 @@ function nav() {
             <div className="nav">
                 <img src={logo} className='logo'></img>
                 <button className='navButton'><Link to="mint" spy={true} smooth={true}>Mint</Link></button>
-                <button className='navButton'><Link to="team" spy={true} smooth={true}>Team</Link></button>
+                <button className='navButton'><Link to="TeamContainer" spy={true} smooth={true}>Team</Link></button>
                 <button className='navButton'><Link to="FQAMainContainer" spy={true} smooth={true}>About</Link></button>
                 <button className='navButton'><Link to="Team" spy={true} smooth={true}>Roadmap</Link></button>
                 <a href="https://twitter.com/DoodleNoodleNFT" target='_blank'
