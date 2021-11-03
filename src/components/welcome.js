045a002359971d651374d3f3c821b98467316b66
callum buttery <callumbuttery@icloud.com>
2021-11-03 17:37:46 +0000

    welcome background size and spinning logo fix

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -4,13 +4,13 @@ import Timer from '../components/Timer';
 
 function welcome() {
 
+    <Timer launchDate='November, 21, 2021'></Timer>
     return (
         <div className='mint'>
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h1 style={{ color: '#FF123F' }}>Doodle Noodles coming soon</h1>
-                    <Timer launchDate='November, 21, 2021'></Timer>
+                    <h1 style={{ color: '#FF123F', fontSize: '50px', marginTop: "20px" }}>Coming Soon!!</h1>
                 </header>
             </div>
         </div >
