6c40d72a6463ee9d9bdc7f072ab8443b37712611
callum buttery <callumbuttery@icloud.com>
2021-11-11 22:39:27 +0000

    Centre the world, minus we are doodle noodles

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -5,26 +5,28 @@ function FAQ() {
     return (
         <div className='FQAMainContainer'>
             <div className='secondaryContainer'>
-                <img src={FAQimg} className='FQALogo' alt="FQA"></img>
-                <div className="FQAcontent">
-                    <h2 className='listItem'>1. When will the Noodles launch?</h2>
-                    <p className='FQAParagraphFont'>We will be having a presale for our whitelisted members
-                        of the first 3000 Noodles. Then a few days later the public sale will go live.
-                        <b>The date of the presale will be announced on our Discord & Twitter!</b>
-                    </p>
-                    <h2 className='listItem'>2. How can I join the whitelist for Pre-Sale?</h2>
-                    <p className='FQAParagraphFont'>For more information on the events where you can join
-                        the whitelist, keep up to date and active on this server and on our twitter!
-                    </p>
-                    <h2 className='listItem'>3. What is the mint price & Limit?</h2>
-                    <p className='FQAParagraphFont'>It's important to us that the community has their say on
-                        these and so the mint price and the mint limit per wallet address will be confirmed closer
-                        to the launch date by our team.
-                    </p>
-                    <h2 className='listItem'>4. Do we own the licences to our Noodles?</h2>
-                    <p className='FQAParagraphFont'>Yes! You are free to use the Noodles you have purchased
-                        however you wish - however the only way to transfer the licence is through selling.
-                    </p>
+                <div className='FQA-header'>
+                    <img src={FAQimg} className='FQALogo' alt="FQA"></img>
+                    <div className="FQAcontent">
+                        <h2 className='listItem'>1. When will the Noodles launch?</h2>
+                        <p className='FQAParagraphFont'>We will be having a presale for our whitelisted members
+                            of the first 3000 Noodles. Then a few days later the public sale will go live.
+                            <b>The date of the presale will be announced on our Discord & Twitter!</b>
+                        </p>
+                        <h2 className='listItem'>2. How can I join the whitelist for Pre-Sale?</h2>
+                        <p className='FQAParagraphFont'>For more information on the events where you can join
+                            the whitelist, keep up to date and active on this server and on our twitter!
+                        </p>
+                        <h2 className='listItem'>3. What is the mint price & Limit?</h2>
+                        <p className='FQAParagraphFont'>It's important to us that the community has their say on
+                            these and so the mint price and the mint limit per wallet address will be confirmed closer
+                            to the launch date by our team.
+                        </p>
+                        <h2 className='listItem'>4. Do we own the licences to our Noodles?</h2>
+                        <p className='FQAParagraphFont'>Yes! You are free to use the Noodles you have purchased
+                            however you wish - however the only way to transfer the licence is through selling.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,20 +1,24 @@
 import '../css/Team.css'
 function Team() {
     return (
-        <div className='TeamContainer'>
-            <h1 className="TeamTitle">Meet the team</h1>
-            <hr className="TeamHR" />
-            <p className="TeamText" style={{ paddingBottom: 0 }}>We are two ordinary Scottish guys, passionate
-                about NFTs and the world of blockchain, looking
-                to innovate with our project and bring something
-                new to the space
+        <div className="teamMainContainer">
+            <div className='TeamContainer'>
+                <div className="Team-header">
+                    <h1 className="TeamTitle">Meet the team</h1>
+                    <hr className="TeamHR" />
+                    <p className="TeamText" style={{ paddingBottom: 0 }}>We are two ordinary Scottish guys, passionate
+                        about NFTs and the world of blockchain, looking
+                        to innovate with our project and bring something
+                        new to the space
 
-                <br />
-                <br />
-                The aim of this project is to build an amazing
-                community based around creativity and innovation
-                and above all - having fun together
-            </p>
+                        <br />
+                        <br />
+                        The aim of this project is to build an amazing
+                        community based around creativity and innovation
+                        and above all - having fun together
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
