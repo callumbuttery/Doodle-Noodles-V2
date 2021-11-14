f15e7cfaafcdbc7bc983de1846123736b08e1e63
callum buttery <callumbuttery@icloud.com>
2021-11-14 18:47:01 +0000

    Fix rune text size

--- src/components/Rune.js
+++ src/components/Rune.js
@@ -5,26 +5,28 @@ function Rune() {
         <div className="RuneContainer">
             <div className="RuneSecondaryContainer">
                 <div className="Rune-header">
-                    <h3 className="RuneTitle">Rune System</h3>
-                    <p className="RuneText">
-                        Our Golden Rune system allows
-                        5 lucky minters to have a
-                        customer Doodle Noodle created
-                        and minted for them to their
-                        own specifications yb our artists
-                        Grin!
-                        <br></br>
-                        <br></br>
-                        Every Doodle Noodle will have
-                        an associated rune, which will
-                        be used in future for
-                        competitions and giveaways.
-                        But if you mint and find a
-                        Golden Rune on your NFT - you
-                        should start thinking about
-                        what your ideal Doodle Noodle
-                        would look like!
-                    </p>
+                    <div className="RuneTextContainers">
+                        <h3 className="RuneTitle">Rune System</h3>
+                        <p className="RuneText">
+                            Our Golden Rune system allows
+                            5 lucky minters to have a
+                            customer Doodle Noodle created
+                            and minted for them to their
+                            own specifications yb our artists
+                            Grin!
+                            <br></br>
+                            <br></br>
+                            Every Doodle Noodle will have
+                            an associated rune, which will
+                            be used in future for
+                            competitions and giveaways.
+                            But if you mint and find a
+                            Golden Rune on your NFT - you
+                            should start thinking about
+                            what your ideal Doodle Noodle
+                            would look like!
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
