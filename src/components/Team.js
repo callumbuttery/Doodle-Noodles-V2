d82d4d38bb6dca54cadc480e9e0e42dffc4f48f4
callum buttery <callumbuttery@icloud.com>
2021-11-15 21:52:48 +0000

    Team text changes

--- src/components/Team.js
+++ src/components/Team.js
@@ -6,16 +6,13 @@ function Team() {
                 <div className="Team-header">
                     <h1 className="TeamTitle">Meet the team</h1>
                     <hr className="TeamHR" />
-                    <p className="TeamText" style={{ paddingBottom: 0 }}>We are two ordinary Scottish guys, passionate
-                        about NFTs and the world of blockchain, looking
-                        to innovate with our project and bring something
-                        new to the space
+                    <p className="TeamText" style={{ paddingBottom: 0 }}>We are two ordinary Scottish guys, passionate about NFTs and the world of blockchain. 
+                    Grin is our Founder, Artist and Blockchain developer and Buttery is our web wizard dealing with our website!
 
                         <br />
                         <br />
-                        The aim of this project is to build an amazing
-                        community based around creativity and innovation
-                        and above all - having fun together
+                        This project is looking to bring something new to the NFT space and build an amazing community centred around creativity and innovation. 
+                        Let’s have some fun together. 
                     </p>
                 </div>
             </div>
