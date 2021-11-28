dce7bf222da5871e4d37cbba46976bc346d2069a
callum buttery <callumbuttery@icloud.com>
2021-11-28 11:41:40 +0000

    About updates

--- src/components/About.js
+++ src/components/About.js
@@ -1,28 +1,44 @@
-import '../css/About.css'
-import WhatAreDoodleNoodles from '../imgs/WhatAreDoodleNoodles.png'
+import "../css/About.css";
+import WhatAreDoodleNoodles from "../imgs/WhatAreDoodleNoodles.png";
 
 function about() {
-    return (
-        <div className='mainContainer'>
-            <div className='secondaryContainer'>
-                <img src={WhatAreDoodleNoodles} className='WhatAreDoodleNoodlesLogo' alt="Doodle Noodles"></img>
-                <div className="WhatAreDoodleNoodlesContent">
-                    <h1 className='Title'>What are Doodle Noodles?</h1>
-                    <hr className='hr'></hr>
-                    <p className='paragraphFont'>Doodle Noodles are 6666 snake NFTs looking to make Hisstory on the Ethereum blockchain. 
-                    They were created by our founder Grin and generated from billions of possible combinations in order to produce 
-                    a varied collection of amazing art. 
-                    </p>
-                    <p className='paragraphFont'>Some Noodles are rarer than others and some carry a special Golden Rune (more info below). 
-                    Follow our planned journey by looking at our roadmap! For every Noodle minted we plan to plant 3 trees with OneTreePlanted, 
-                    totalling 20,000 trees!
-                    </p>
-                    <p className='paragraphFont'>Join the Twitter and Discord to get involved, we’d love to meet you. 
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className="mainContainer">
+      <div className="secondaryContainer">
+        <img
+          src={WhatAreDoodleNoodles}
+          className="WhatAreDoodleNoodlesLogo"
+          alt="Doodle Noodles"
+        ></img>
+        <div className="WhatAreDoodleNoodlesContent">
+          <h1 className="Title">What are Doodle Noodles?</h1>
+          <hr className="hr"></hr>
+          <p className="paragraphFont">
+            Doodle Noodles are 6666 snake NFTs looking to make Hisstory on the
+            Ethereum blockchain. They were created by our founder Grin and
+            generated from billions of possible combinations in order to produce
+            a varied collection of amazing art.
+          </p>
+          <p className="paragraphFont">
+            -Every Noodle plants three trees with OneTreePlanted!
+            <br/>
+            - Our Doodle Noodle Ramen will drop physical ramen to holders.
+            <br/>
+            - We’re opening a Ramen & Merch store in The Sandbox!
+            <br/>
+            - New Evolving NFTs will be dropped to holders and used in our upcoming Trading Card
+            Game!
+            <br/>
+            - 5 lucky minters will have the chance of getting a Golden Rune! Allowing them to have
+            their own custom Noodle created and minted for them.
+          </p>
+          <p className="paragraphFont">
+            Join the Twitter and Discord to get involved, we’d love to meet you.
+          </p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default about;
