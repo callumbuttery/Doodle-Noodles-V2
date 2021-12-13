07a570baf1be57d4c6e64cfb87cb453c5007bc64
callum buttery <callumbuttery@icloud.com>
2021-12-13 21:36:01 +0000

    Omen format fix

--- src/components/Nav2.js
+++ src/components/Nav2.js
@@ -2,7 +2,7 @@ import logo from '../imgs/Logo.png';
 import '../css/Nav2.css';
 import 'bootstrap/dist/css/bootstrap.css'
 
-import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+import { Link } from 'react-scroll'
 
 import { Nav, Navbar } from 'react-bootstrap'
 import discordIcon from '../imgs/Discord-Logo.png';
@@ -16,21 +16,21 @@ function Nav2() {
             <div className="Nav2SecondaryContainer">
             <Navbar sticky="top" expand="sm" collapseOnSelect className="nav2">
                 <Navbar.Brand>
-                    <img src={logo} width="200px" height="50px" style={{ marginLeft: '50px' }} alt="Doodle Noodles"/>
+                    <img src={logo} rel="noreferrer" width="200px" height="50px" style={{ marginLeft: '50px' }} alt="Doodle Noodles"/>
                 </Navbar.Brand>
 
                 <Navbar.Toggle style={{ backgroundColor: "white" }}></Navbar.Toggle>
                 <Navbar.Collapse>
-                    <Nav className="justify-content-end" style={{ width: "95%" }}>
-                        <Link to="mint" className='NavItem' style={{textDecoration: 'none'}}>Mint</Link>
-                        <Link to="TeamContainer" className='NavItem' style={{textDecoration: 'none'}}>Team</Link>
-                        <Link to="FQAMainContainer" className='NavItem' style={{textDecoration: 'none'}}>About</Link>
-                        <a href="https://medium.com/@doodlenoodles/doodle-noodles-utility-roadmap-7a9feb9953f4" className='NavItem' target="_blank" style={{textDecoration: 'none'}}>Roadmap</a>
-                        <a href="https://discord.com/invite/rReed6g5" target='_blank'
-                        ><img src={discordIcon} className="NavIconDiscord" /></a>
+                    <Nav className="justify-content-end" rel="noreferrer" style={{ width: "95%" }}>
+                        <Link to="mint" className='NavItem' rel="noreferrer" style={{textDecoration: 'none'}}>Mint</Link>
+                        <Link to="TeamContainer" className='NavItem' rel="noreferrer" style={{textDecoration: 'none'}}>Team</Link>
+                        <Link to="FQAMainContainer" className='NavItem' rel="noreferrer" style={{textDecoration: 'none'}}>About</Link>
+                        <a href="https://medium.com/@doodlenoodles/doodle-noodles-utility-roadmap-7a9feb9953f4" className='NavItem' rel="noreferrer" target="_blank" style={{textDecoration: 'none'}}>Roadmap</a>
+                        <a href="https://discord.com/invite/rReed6g5" rel="noreferrer" target='_blank'
+                        ><img src={discordIcon} className="NavIconDiscord" alt="Discord Icon"/></a>
 
-                        <a href="https://twitter.com/DoodleNoodleNFT" target='_blank'
-                        ><img src={twitterIconRed} className="NavIconTwitter" /></a>
+                        <a href="https://twitter.com/DoodleNoodleNFT"  rel="noreferrer" target='_blank'
+                        ><img src={twitterIconRed} className="NavIconTwitter" alt="Twitter Icon"/></a>
                     </Nav>
                 </Navbar.Collapse>
 
