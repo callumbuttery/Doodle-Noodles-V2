b9d2714c7e4f45ac99a04bfaa02f41850f25f27b
callum buttery <callumbuttery@icloud.com>
2022-06-19 17:09:30 +0100

    Feat: new nav bar

--- src/components/Nav2.js
+++ src/components/Nav2.js
@@ -7,6 +7,7 @@ import { Link } from 'react-scroll'
 import { Nav, Navbar } from 'react-bootstrap'
 import discordIcon from '../imgs/Discord-Logo.png';
 import twitterIconRed from '../imgs/twitter-icon-red.png';
+import mediumIcon from '../imgs/medium.png';
 
 
 
@@ -14,27 +15,32 @@ function Nav2() {
     return (
         <div className="Nav2MainContainer">
             <div className="Nav2SecondaryContainer">
-            <Navbar sticky="top" expand="sm" collapseOnSelect className="nav2">
-                <Navbar.Brand>
-                    <img src={logo} rel="noreferrer" width="200px" height="50px" style={{ marginLeft: '50px' }} alt="Doodle Noodles"/>
-                </Navbar.Brand>
+                <Navbar sticky="top" expand="sm" collapseOnSelect className="nav2">
+                    <Navbar.Brand>
+                        <img src={logo} rel="noreferrer" width="150px" style={{ marginLeft: '120px' }} alt="Doodle Noodles" />
+                    </Navbar.Brand>
 
-                <Navbar.Toggle style={{ backgroundColor: "white" }}></Navbar.Toggle>
-                <Navbar.Collapse>
-                    <Nav className="justify-content-end" rel="noreferrer" style={{ width: "95%" }}>
-                        <Link to="mint" className='NavItem' rel="noreferrer" style={{textDecoration: 'none'}}>Mint</Link>
-                        <Link to="TeamContainer" className='NavItem' rel="noreferrer" style={{textDecoration: 'none'}}>Team</Link>
-                        <Link to="FQAMainContainer" className='NavItem' rel="noreferrer" style={{textDecoration: 'none'}}>About</Link>
-                        <a href="https://medium.com/@doodlenoodles/doodle-noodles-utility-roadmap-7a9feb9953f4" className='NavItem' rel="noreferrer" target="_blank" style={{textDecoration: 'none'}}>Roadmap</a>
-                        <a href="https://discord.com/invite/sendnoods" rel="noreferrer" target='_blank'
-                        ><img src={discordIcon} className="NavIconDiscord" alt="Discord Icon"/></a>
+                    <Navbar.Toggle style={{ backgroundColor: "white" }}></Navbar.Toggle>
+                    <Navbar.Collapse>
+                        <Nav className="justify-content-centre" rel="noreferrer" style={{ width: "100%" }}>
+                            <Link to="mint" className='NavItem' rel="noreferrer" style={{ textDecoration: 'none' }}>Mint</Link>
+                            <Link to="TeamContainer" className='NavItem' rel="noreferrer" style={{ textDecoration: 'none' }}>Roadmap</Link>
+                            <Link to="FQAMainContainer" className='NavItem' rel="noreferrer" style={{ textDecoration: 'none' }}>FAQ</Link>
+                            <a href="https://medium.com/@doodlenoodles/doodle-noodles-utility-roadmap-7a9feb9953f4" className='NavItem' rel="noreferrer" target="_blank" style={{ textDecoration: 'none' }}>Roadmap</a>
 
-                        <a href="https://twitter.com/DoodleNoodleNFT"  rel="noreferrer" target='_blank'
-                        ><img src={twitterIconRed} className="NavIconTwitter" alt="Twitter Icon"/></a>
-                    </Nav>
-                </Navbar.Collapse>
+                            <div style={{ marginLeft: '10px'}}>
+                                <a href="https://twitter.com/DoodleNoodleNFT" rel="noreferrer" target='_blank'
+                                ><img src={twitterIconRed} className="NavIconTwitter" alt="Twitter Icon" /></a>
+                                <a href="https://discord.com/invite/sendnoods" rel="noreferrer" target='_blank'
+                                ><img src={discordIcon} className="NavIconDiscord" alt="Discord Icon" /></a>
+                                <a href="https://medium.com/@doodlenoodles" rel="noreferrer" target='_blank'
+                                ><img src={mediumIcon} className="NavIconMedium" alt="Medium Icon" /></a>
+                            </div>
 
-            </Navbar>
+                        </Nav>
+                    </Navbar.Collapse>
+
+                </Navbar>
             </div>
         </div>
     );
