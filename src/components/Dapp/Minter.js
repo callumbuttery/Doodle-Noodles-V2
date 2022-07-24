e87964431e45e221d9bf1311260c3bd2da4fff3d
callum buttery <callumbuttery@icloud.com>
2022-07-24 15:23:01 +0100

    Feat: add mint UI

--- src/components/Dapp/Minter.js
+++ src/components/Dapp/Minter.js
@@ -4,9 +4,22 @@ import { MDBBtn } from 'mdb-react-ui-kit'
 
 
 function Minter() {
+
+    const flipComponents = () => {
+        document.getElementById("beginMintingButton").style.display = "none";
+        document.getElementById("mintingProcessButtons").style.display = "flex";
+        document.getElementById("mintButton").style.display = "flex";
+    }
+
     return (
         <div dappContainer>
-            <MDBBtn className='mx-4 mintButton' color='danger'>Begin Minting</MDBBtn>
+            <MDBBtn id="beginMintingButton" className='mx-4 beginMintingButton' color='danger' onClick={flipComponents}>Begin Minting</MDBBtn>
+            <div id="mintingProcessButtons">
+                <MDBBtn className='minusButton' color='danger'>-</MDBBtn>
+                <div id="mintNumber">5</div>
+                <MDBBtn className='plusButton' color='danger'>+</MDBBtn>
+            </div>
+            <MDBBtn id='mintButton' color='warning' size="lg">Mint</MDBBtn>
         </div >
     )
 }
