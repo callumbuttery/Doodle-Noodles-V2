e81f48bc8de99d1cd31bf1d940ab9ec1fdee9e65
callum buttery <callumbuttery@icloud.com>
2022-07-24 14:28:01 +0100

    Feat: new minter background changes

--- src/components/Dapp/Minter.js
+++ src/components/Dapp/Minter.js
@@ -1,33 +1,13 @@
+import "../../css/minter.css";
 import "../../css/App.css";
-import styled from "styled-components";
-import { ethers } from "ethers";
-import logo from "../../imgs/Logo2.png";
-import axios from "axios";
-import Web3 from "web3";
-import React, { useEffect, useState } from "react";
-import * as card from "../../css/Styles";
+import { MDBBtn } from 'mdb-react-ui-kit'
+
 
 function Minter() {
     return (
         <div dappContainer>
-            <card.card>
-                    <card.Container flex={1} ai={"center"} justifyContent={"center"}>
-                        <card.Title>MINT HERE</card.Title>
-                        <card.Description>Each Noodle is 0.06Ξ. Mint limit is 5 per wallet.</card.Description>
-                        <card.SpacerSmall />
-                        <button
-                            onClick={(e) => {
-                                e.preventDefault();
-                            }}
-                            className="mintButton"
-                        >
-                            CONNECT
-                        </button>
-                        <card.SpacerSmall />
-                    </card.Container>
-            </card.card>
-
-        </div>
+            <MDBBtn className='mx-4 mintButton' color='danger'>Begin Minting</MDBBtn>
+        </div >
     )
 }
 
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,12 +1,7 @@
-import logo from '../imgs/Logo3.png'
 import '../css/welcome.css';
-import Timer from '../components/Timer';
-import Dapp from '../dapp.js'
 import Minter from '../components/Dapp/Minter';
 
 function welcome() {
-
-    <Timer launchDate='November, 21, 2021'></Timer>
     return (
         <div className='mint'>
             <div className="App">
